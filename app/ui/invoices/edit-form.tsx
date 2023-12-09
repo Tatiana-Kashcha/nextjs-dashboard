75ec0f48642683914293cec7d2928a1a1b78a11a
Tatiana Kashcha <tkashcha@gmail.com>
2023-12-09 11:17:18 +0200

    Update edit-form.tsx

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -11,7 +11,7 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice } from '@/app/lib/actions';
-
+//тест1
 export default function EditInvoiceForm({
   invoice,
   customers,
